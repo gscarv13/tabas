f3496cddc3af3523ccb61a889130a8b7f4acebce
Gustavo Carvalho <gsc13@tuta.io>
2022-09-16 18:19:04 -0300

    Add seed data

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -1,7 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+def attach_picture(property, path)
+  filename = path.split('/').last
+
+  property.pictures.attach(io: File.open(path), filename: filename, content_type: 'image/jpeg')
+end
+
+pictures_set_1 = [
+  'app/assets/images/a1.jpg',
+  'app/assets/images/a2.jpg',
+  'app/assets/images/a3.jpg',
+  'app/assets/images/a4.jpg',
+  'app/assets/images/a5.jpg'
+]
+
+pictures_set_2 = [
+  'app/assets/images/b1.jpg',
+  'app/assets/images/b2.jpg',
+  'app/assets/images/b3.jpg'
+]
+
+pictures_set_3 = [
+  'app/assets/images/c1.jpg',
+  'app/assets/images/c2.jpg',
+  'app/assets/images/c3.jpg',
+  'app/assets/images/c4.jpg'
+]
+
+sets = [ pictures_set_1, pictures_set_2, pictures_set_3 ]
+
+puts 'Creating properties...'
+
+50.times do |iteration|
+  property = Property.create(
+    name: Faker::Cosmere.aon,
+    address: "#{Faker::Address.street_address} #{Faker::Address.city}",
+    description: Faker::Lorem.paragraph_by_chars(number: 150),
+    monthly_rate: (rand(10.0 .. 35.0) * 1000).round(2)
+  )
+
+  pictures_set = sets[ iteration % 3 ]
+  pictures_set.each { |img_path|  attach_picture(property, img_path) }
+end
+
+puts 'Done!'
