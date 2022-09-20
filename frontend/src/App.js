d5dab6819d01a78214cebf4049ef47a4034ea6d5
Gustavo Carvalho <gsc13@tuta.io>
2022-09-19 23:43:44 -0300

    Update App Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,11 @@
-// import { getRequest } from './utils/requests'
-import PropertyList from './components/PropertyList';
+import Header from './components/Header/Header';
+import PropertyList from './components/PropertyList/PropertyList';
 
 function App() {
   return (
     <div>
-      <header>
-        <span>LOGO</span>
-        <div>
-          <p>Seu apartamento mobiliado pronto para morar</p>
-          <p>Apartamentos completos em São Paulo, Rio de Janeiro e Brasília desenhados para você se sentir em casa</p>
-        </div>
-      </header>
-      <main className="asd">
-        <p className="title">Tenha a experiencia de um Edifício Tabas</p>
+      <Header />
+      <main>
         <PropertyList />
       </main>
     </div>
