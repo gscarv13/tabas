95617855e8832665ecdc222715a9081260a59cf7
Gustavo Carvalho <gsc13@tuta.io>
2022-09-19 23:30:47 -0300

    Update images and seed logic

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -1,7 +1,7 @@
 def attach_picture(property, path)
   filename = path.split('/').last
 
-  property.pictures.attach(io: File.open(path), filename: filename, content_type: 'image/jpeg')
+  property.pictures.attach(io: File.open(path), filename: "#{filename}-#{Random.seed}", content_type: 'image/jpeg')
 end
 
 pictures_set_1 = [
@@ -25,19 +25,33 @@ pictures_set_3 = [
   'app/assets/images/c4.jpg'
 ]
 
-sets = [ pictures_set_1, pictures_set_2, pictures_set_3 ]
+pictures_set_4 = [
+  'app/assets/images/d1.jpg',
+  'app/assets/images/d2.jpg',
+  'app/assets/images/d3.jpg',
+  'app/assets/images/d4.jpg'
+]
+
+pictures_set_5 = [
+  'app/assets/images/e1.jpg',
+  'app/assets/images/e2.jpg',
+  'app/assets/images/e3.jpg',
+  'app/assets/images/e4.jpg'
+]
+
+sets = [ pictures_set_1, pictures_set_2, pictures_set_3, pictures_set_4, pictures_set_5 ]
 
 puts 'Creating properties...'
 
 50.times do |iteration|
   property = Property.create(
     name: Faker::Cosmere.aon,
-    address: "#{Faker::Address.street_address} #{Faker::Address.city}",
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
     description: Faker::Lorem.paragraph_by_chars(number: 150),
     monthly_rate: (rand(10.0 .. 35.0) * 1000).round(2)
   )
 
-  pictures_set = sets[ iteration % 3 ]
+  pictures_set = sets[ iteration % 5 ]
   pictures_set.each { |img_path|  attach_picture(property, img_path) }
 end
 
